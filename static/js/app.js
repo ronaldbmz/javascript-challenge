17b7c8ac3d2ecfb9459a61382decbf2cf62d3294
ronaldbmz <tiffanyelle@Tiffanys-iMac.local>
2021-04-11 18:46:20 +0200

    added date filter

--- static/js/app.js
+++ static/js/app.js
@@ -38,5 +38,64 @@ html+="</tr>";
 //to send html tags and data to html frontend
 d3.select("table>tbody").html(html);
 
+// ************* Showing data based on date filter ********************
+//Date filtering function
+function filterDate(data, inputValue) {
+
+	// Select the input date element and get the raw HTML node
+	var inputDate = d3.select("#datetime");
+	// Get the date value property of the input element
+	var inputDateValue = inputDate.property("value");
+	//console.log(inputDateValue);
+
+	if (inputDateValue === "") {
+		return data;
+	} else {
+		 return data.datetime === inputDateValue;
+	}
+
+}
+
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+	
+	// Prevent the page from refreshing
+	d3.event.preventDefault();
+	
+	// Use the form input to filter the data
+	var rows = tableData.filter(filterDate);
+	
+	
+	html = "";
+	
+	for (var i = 0; i < rows.length; i++) {
+	html+="<tr>";
+	html+="<td>"+rows[i].datetime+"</td>";
+	html+="<td>"+toCamelCase(rows[i].city)+"</td>";
+	html+="<td>"+rows[i].state.toUpperCase()+"</td>";
+	html+="<td>"+rows[i].country.toUpperCase()+"</td>";
+	html+="<td>"+toCamelCase(rows[i].shape)+"</td>";
+	html+="<td>"+rows[i].durationMinutes+"</td>";
+	html+="<td>"+toCamelCase(rows[i].comments)+"</td>";
+	html+="</tr>";
+
+	}
+	d3.select("table>tbody").html(html);
+ 
+
+ 
+	
+};
 
 
