f4a6c3887da1e131d46eb96f67dcd718ac17799d
ronaldbmz <tiffanyelle@Tiffanys-iMac.local>
2021-04-11 18:12:40 +0200

    populated data on html page

--- static/js/app.js
+++ static/js/app.js
@@ -2,8 +2,40 @@
 var tableData = data;
 
 // view the ufo data on console
-console.log(tableData)
+//console.log(tableData);
 
+var rows = tableData;
+
+// function to create text into CamelCase
+function toCamelCase(str) {
+    return str
+        .replace(/\s(.)/g, function($1) { return $1.toUpperCase(); })
+        .replace(/^(.)/, function($1) { return $1.toUpperCase(); });
+}
+
+console.log(toCamelCase("benton"));
+
+	
+html = "";
+
+for (var i = 0; i < rows.length; i++) {
+html+="<tr>";
+html+="<td>"+rows[i].datetime+"</td>";
+html+="<td>"+rows[i].city+"</td>";
+html+="<td>"+rows[i].state+"</td>";
+html+="<td>"+rows[i].country+"</td>";
+html+="<td>"+rows[i].shape+"</td>";
+html+="<td>"+rows[i].durationMinutes+"</td>";
+html+="<td>"+rows[i].comments+"</td>";
+html+="</tr>";
+
+}
+
+// view the ufo data on console
+//console.log(html);
+
+//to send html tags and data to html frontend
+d3.select("table>tbody").html(html);
 
 
 
